d9407056ab915d0f12e2f67416873c0b0a033008
Gosila <goshila.sadaf@therightsw.com>
2024-05-10 19:00:27 +0500

    initial commit

---
+++ cointopay/vendor/cointopay/lib/Merchant/Order.php
@@ -0,0 +1,40 @@
+<?php
+namespace cointopay\Merchant;
+
+use cointopay\Cointopay;
+use cointopay\Merchant;
+
+class Order extends Merchant
+{
+    private $order;
+
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    public static function createOrFail($params, $options = array(), $authentication = array())
+    {
+        $order = Cointopay::request('orders', 'GET', $params, $authentication);
+		if (is_string($order) && $order != 'testmerchant success'){
+			return $order;
+		} else {
+			return new self($order);
+		}
+    }
+	public static function ValidateOrder($params, $options = array(), $authentication = array())
+    {
+        $order = Cointopay::request('validation', 'GET', $params, $authentication);
+        return new self($order);
+    }
+
+    public function toHash()
+    {
+        return $this->order;
+    }
+
+    public function __get($name)
+    {
+        return $this->order[$name];
+    }
+}
